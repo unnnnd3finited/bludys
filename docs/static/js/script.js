57f1904ea3e0c031c2760e016b9338fcc120a05f
unnnnd3finited <papafrancy69104@gmail.com>
2025-07-10 21:55:12 +0200

    Update script.js

--- docs/static/js/script.js
+++ docs/static/js/script.js
@@ -22,6 +22,14 @@ const songs = [
     weeklySession: true,
   },
     {
+    title: "Pretty Girl (slowed + reverb)",
+    artist: "Clairo",
+    src: "static/songs/pretty.mp3",
+    plays: 0,
+    favorite: true,
+    weeklySession: true,
+  },
+    {
     title: "bakground",
     artist: "Gotham love",
     src: "static/songs/bak.mp3",
